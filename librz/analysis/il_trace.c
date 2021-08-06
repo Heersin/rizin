db646246a0123ef6e4a737d12ef7366851c34fea
Heersin <teablearcher@gmail.com>
2021-08-06 17:34:55 +0800

    Dump regname before esil emul to prevent UAF

--- librz/analysis/il_trace.c
+++ librz/analysis/il_trace.c
@@ -16,6 +16,17 @@
 #include <rz_util.h>
 #include <rz_analysis.h>
 
+static void free_reg_op(RzILTraceRegOp *regop) {
+	if (regop) {
+		if (regop->reg_name) {
+                        free(regop->reg_name);
+                        regop->reg_name = NULL;
+		}
+
+		RZ_FREE(regop);
+	}
+}
+
 /**
  * create and init a trace structure for an instruction at address
  * \param addr ut64, address of instruction
@@ -31,9 +42,9 @@ RZ_API RzILTraceInstruction *rz_analysis_il_trace_instruction_new(ut64 addr) {
 	instruction_trace->addr = addr;
 
 	instruction_trace->read_mem_ops = rz_pvector_new((RzPVectorFree)free);
-	instruction_trace->read_reg_ops = rz_pvector_new((RzPVectorFree)free);
+	instruction_trace->read_reg_ops = rz_pvector_new((RzPVectorFree)free_reg_op);
 	instruction_trace->write_mem_ops = rz_pvector_new((RzPVectorFree)free);
-	instruction_trace->write_reg_ops = rz_pvector_new((RzPVectorFree)free);
+	instruction_trace->write_reg_ops = rz_pvector_new((RzPVectorFree)free_reg_op);
 	// TODO : handle error
 	return instruction_trace;
 }
